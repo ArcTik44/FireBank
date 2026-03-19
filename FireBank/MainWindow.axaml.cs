dbf8767221d42cb1c6bf424599f5c71b083938cc
Šimon Hyksa <simonhyksa6@gmail.com>
2026-03-19 12:48:28 +0100

    Přidejte soubory projektu.

---
+++ FireBank/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FireBank
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
